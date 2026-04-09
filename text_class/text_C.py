9e2f309f4bffd8a8394352d516ad1f9fac79eee9
zhoule6665 <zhoule6665@gmail.com>
2026-04-09 22:33:53 +0800

    2026.4.9

---
+++ text_class/text_C.py
@@ -0,0 +1,5 @@
+import text_class
+if __name__=="__main__":
+    print("11")
+    stu1 =text_class.Student("dr",100,18,20)
+    print(stu1.name)
---
+++ text_class/text_class.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,a,b,c,d):
+        self.name = a
+        self.sex = b
+        self.age = c
+        self.height = d
+def listen(self):
+    print("听课")
+def write(self):
+    print(self)
+
